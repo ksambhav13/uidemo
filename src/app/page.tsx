ebaea1d4ccf4332c48ae81315ffe8e9a886cc1a1
Sambhav <sambhav@skillate.com>
2023-11-28 17:16:39 +0530

    implement signout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-'use client'
-import { useAuth } from '@/AuthContext'
-import withAuth from '@/withAuth'
+"use client";
+import { useAuth } from "@/AuthContext";
+import withAuth from "@/withAuth";
+import { Button } from "@chakra-ui/react";
+import axios from "axios";
 
 export default withAuth(function Home() {
-  const { user } = useAuth()
+  const { user, setUser } = useAuth();
+  const signOut = () => {
+    axios.post("/api/auth/logout").then(() => {
+      setUser(null);
+    });
+  };
   return (
     <main>
-      Welcome {user?.username}
+      Welcome {user?.username} to the app!
+      <div>
+        <Button onClick={signOut}>Sign Out</Button>
+      </div>
     </main>
-  )
-})
+  );
+});
