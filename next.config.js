29892366948b35392ab45c928408abdfc7af7f5a
Sambhav <sambhav@skillate.com>
2023-11-28 16:22:52 +0530

    working authentication

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const backendEndpoint = "http://127.0.0.1:8080";
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${backendEndpoint}/api/:path*`,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
--- src/app/auth/login/App.tsx
+++ src/app/auth/login/App.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import {
   Box,
   Button,
@@ -13,57 +13,108 @@ import {
   Link,
   Stack,
   Text,
-} from '@chakra-ui/react'
-import { Logo } from './Logo'
-import { OAuthButtonGroup } from './OAuthButtonGroup'
-import { PasswordField } from './PasswordField'
+} from "@chakra-ui/react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-export const App = () => (
-  <Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }}>
-    <Stack spacing="8">
-      <Stack spacing="6">
-        <Logo />
-        <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
-          <Heading size={{ base: 'xs', md: 'sm' }}>Log in to your account</Heading>
-          <Text color="fg.muted">
-            Don't have an account? <Link href="#">Sign up</Link>
-          </Text>
-        </Stack>
-      </Stack>
-      <Box
-        py={{ base: '0', sm: '8' }}
-        px={{ base: '4', sm: '10' }}
-        bg={{ base: 'transparent', sm: 'bg.surface' }}
-        boxShadow={{ base: 'none', sm: 'md' }}
-        borderRadius={{ base: 'none', sm: 'xl' }}
-      >
+import { Logo } from "./Logo";
+import { OAuthButtonGroup } from "./OAuthButtonGroup";
+import { PasswordField } from "./PasswordField";
+import axios from "axios";
+import { useAuth } from "@/AuthContext";
+import { useRouter } from "next/navigation";
+
+type Inputs = {
+  username: string;
+  password: string;
+};
+
+export const App = () => {
+  const { user, setUser } = useAuth();
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      username: "adam@skillate.com",
+      password: "orion123",
+    },
+  });
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    const formData = new FormData();
+    formData.append("username", data.username);
+    formData.append("password", data.password);
+    axios
+      .post("/api/auth/login", formData, { withCredentials: true })
+      .then(() => {
+        console.log(data);
+        setUser(data);
+      });
+  };
+  if (user) {
+    router.push("/");
+  }
+  return (
+    <Container
+      maxW="lg"
+      py={{ base: "12", md: "24" }}
+      px={{ base: "0", sm: "8" }}
+    >
+      <Stack spacing="8">
         <Stack spacing="6">
-          <Stack spacing="5">
-            <FormControl>
-              <FormLabel htmlFor="email">Email</FormLabel>
-              <Input id="email" type="email" />
-            </FormControl>
-            <PasswordField />
-          </Stack>
-          <HStack justify="space-between">
-            <Checkbox defaultChecked>Remember me</Checkbox>
-            <Button variant="text" size="sm">
-              Forgot password?
-            </Button>
-          </HStack>
-          <Stack spacing="6">
-            <Button>Sign in</Button>
-            <HStack>
-              <Divider />
-              <Text textStyle="sm" whiteSpace="nowrap" color="fg.muted">
-                or continue with
-              </Text>
-              <Divider />
-            </HStack>
-            <OAuthButtonGroup />
+          <Logo />
+          <Stack spacing={{ base: "2", md: "3" }} textAlign="center">
+            <Heading size={{ base: "xs", md: "sm" }}>
+              Log in to your account
+            </Heading>
+            <Text color="fg.muted">
+              Don't have an account? <Link href="#">Sign up</Link>
+            </Text>
           </Stack>
         </Stack>
-      </Box>
-    </Stack>
-  </Container>
-)
+        <Box
+          py={{ base: "0", sm: "8" }}
+          px={{ base: "4", sm: "10" }}
+          bg={{ base: "transparent", sm: "bg.surface" }}
+          boxShadow={{ base: "none", sm: "md" }}
+          borderRadius={{ base: "none", sm: "xl" }}
+        >
+          <Stack spacing="6">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Stack spacing="5">
+                <Stack spacing="5">
+                  <FormControl>
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <Input id="email" type="email" {...register("username")} />
+                  </FormControl>
+                  <PasswordField {...register("password")} />
+                </Stack>
+                <HStack justify="space-between">
+                  <Checkbox defaultChecked>Remember me</Checkbox>
+                  <Button variant="text" size="sm">
+                    Forgot password?
+                  </Button>
+                </HStack>
+                <Stack spacing="6">
+                  <Button type="submit">Sign in</Button>
+                </Stack>
+              </Stack>
+            </form>
+            <Stack spacing="6">
+              <HStack>
+                <Divider />
+                <Text textStyle="sm" whiteSpace="nowrap" color="fg.muted">
+                  or continue with
+                </Text>
+                <Divider />
+              </HStack>
+              <OAuthButtonGroup />
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Container>
+  );
+};
